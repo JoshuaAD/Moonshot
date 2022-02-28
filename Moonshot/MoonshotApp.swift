b75dd796f1f7c3c3c6993ecfb7d1e9702f879e76
JoshuaAD <jdaniels0465@gmail.com>
2022-02-28 10:26:06 -0500

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Joshua Daniels on 2/20/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
